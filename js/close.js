c56134ac770cc1926c65832f2f3f38af27e7c174
Студент <student@KTUIS.LOCAL>
2024-11-21 10:33:10 +0300

    initialization

---
+++ js/close.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    document.getElementById('pop').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function del() {
+    document.getElementById('pop').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
